9c1858ea5e77fab6d0124b180300006e54108a80
suryata <madeputra415@gmail.com>
2023-09-11 09:05:26 +0700

    percobaan

--- home_shop/urls.py
+++ home_shop/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from home_shop.views import index
+
 urlpatterns = [
+    path('',index),
     path('admin/', admin.site.urls),
     path('main/', include('main.urls')),
 ]
