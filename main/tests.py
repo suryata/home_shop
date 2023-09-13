5e8ba337d57340d7a09e5b80cebf4937653ac1df
suryata <madeputra415@gmail.com>
2023-09-13 11:28:09 +0700

    Penambahan test assertequals

--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase, Client
+from main.models import Item
 
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
@@ -8,3 +9,21 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+        
+    #Penambahan test.py
+    def setUp(self):
+        Item.objects.create(name="semen",amount="500",
+                        description="semen terbaik yang dijual",price="45000")
+        Item.objects.create(name="kayu",amount="700",
+                        description="kayu merupakan hal yang penting dalam membuat rumah",price="20000")
+    
+    def test_items_can_created(self):
+        semen = Item.objects.get(name="semen")
+        kayu = Item.objects.get(name="kayu")
+        self.assertEqual(semen.name, "semen")
+        self.assertEqual(kayu.name, "kayu")
+        self.assertEqual(semen.amount, 500)
+        self.assertEqual(kayu.amount, 700)
+        self.assertEqual(semen.price, 45000)
+        self.assertEqual(kayu.price, 20000)
+        
