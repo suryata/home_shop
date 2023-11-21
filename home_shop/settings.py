00a344f3c481be9813904d52e8f8e40c408e0576
suryata <madeputra415@gmail.com>
2023-11-21 18:51:25 +0700

    css

--- home_shop/settings.py
+++ home_shop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home_shop',
     'main',
     'about',
 ]
@@ -128,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Define the directory where your static files are stored
