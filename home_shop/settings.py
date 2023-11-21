5879eef587d4450a6cf431e22d1a791d74f626c2
suryata <madeputra415@gmail.com>
2023-11-21 18:22:00 +0700

    coba coba css

--- home_shop/settings.py
+++ home_shop/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Define the directory where your static files are stored
