29fe076cfa5fb64cadbb462b1b22d92b43614b4a
suryata <madeputra415@gmail.com>
2023-11-21 16:04:50 +0700

    coba css

--- home_shop/settings.py
+++ home_shop/settings.py
@@ -130,9 +130,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Define the directory where your static files are stored
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
