487326e3b4767a1d402056a1eba14ee70b27d337
suryata <madeputra415@gmail.com>
2023-11-22 11:46:50 +0700

    coba coba css

--- home_shop/settings.py
+++ home_shop/settings.py
@@ -149,7 +149,7 @@ STATIC_URL = 'collected_static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
