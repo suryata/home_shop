a978a4bf203e83938fe07d76e3a8daf675017c8f
suryata <madeputra415@gmail.com>
2023-11-22 14:33:34 +0700

    coba css

--- home_shop/urls.py
+++ home_shop/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from home_shop.views import register, login_user
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 from . import views
@@ -13,4 +15,4 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login_user, name='login'),
     path('auth/', include('authentication.urls')),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
